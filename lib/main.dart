55d8137cd5cf5f9fd74bd80c50968af6abfc1119
ahmadhoujayrie <ahmadhoujayrie306@gmail.com>
2021-11-19 22:23:55 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+
+// rendering url 
+String url ="https://marketsdebate.com";
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Webview Example',
+      theme: ThemeData.dark(),
+      routes: {
+        "/": (_) => Home(),
+        "/webview": (_) => WebviewScaffold(
+              url: url,
+              appBar: AppBar(
+                title: Text("WebView"),
+              ),
+              withJavascript: true,
+              withLocalStorage: true,
+              withZoom: true,
+            )
+      },
+
+      // home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  HomeState createState() => HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeState extends State<Home> {
+  final webView = FlutterWebviewPlugin();
+  TextEditingController controller = TextEditingController(text: url);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    webView.close();
+    controller.addListener(() {
+      url = controller.text;
     });
   }
 
   @override
+  void dispose() {
+    webView.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+  // Future launchURL(String url) async {
+  //   if (await canLaunch(url)) {
+  //     await launch(url, forceSafariVC: true, forceWebView: true);
+  //   } else {
+  //     print("Can't Launch ${url}");
+  //   }
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("WebView"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: controller,
+                ),
+              ),
+              RaisedButton(
+                child: Text("Open Webview"),
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/webview");
+                },
+              )
+            ],
+          ),
+        )
+
+        // body: Center(
+        //   child: Column(
+        //     children: <Widget>[
+        //       Container(
+        //         padding: EdgeInsets.all(10.0),
+        //         child: Text(URL),
+        //       ),
+        //       RaisedButton(
+        //         child: Text("Open Link"),
+        //         onPressed: () {
+        //           launchURL(URL);
+        //         },
+        //       )
+        //     ],
+        //   ),
+        // ),
+        );
   }
 }
